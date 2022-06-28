5fa87c822e72569b7df953f2eea9c257f44d352f
Manuel Pfeuffer <mnl.pfeuffer@gmail.com>
2022-06-28 17:36:32 +0200

    Update publication year

--- R/elastes.R
+++ R/elastes.R
@@ -6,7 +6,7 @@
 #' a novel approach for elastic shape mean estimation, where planar curves are
 #' treated as complex functions and a full Procrustes mean is estimated from the
 #' corresponding smoothed hermitian covariance surface, which is combined with the
-#' methods for elastic mean estimation proposed in Steyer, Stöcker, Greven	(2021).
+#' methods for elastic mean estimation proposed in Steyer, Stöcker, Greven	(2022).
 #' See Stöcker et. al. (2022) for details on the method.
 #'
 #' Compute a mean for a set of observed curves: \code{\link{compute_elastic_shape_mean}}
